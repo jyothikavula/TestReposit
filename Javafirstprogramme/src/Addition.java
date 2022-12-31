16e047d94a75c54de44fb7c897a017cfaf507e26
Admin <Admin@DESKTOP-5NRVF8R.mynet>
2022-12-31 19:12:33 +0100

    first commi

---
+++ Javafirstprogramme/src/Addition.java
@@ -0,0 +1,11 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=21;
+		System.out.println("sum of a and b is " + (a+b)); 
+	}
+
+}
---
+++ Javafirstprogramme/src/Comparision.java
@@ -0,0 +1,11 @@
+
+public class Comparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10; int y=20; int z=30;
+if(x<y||x<z);
+
+	}
+
+}
